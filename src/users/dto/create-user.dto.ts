58454250de3c4e7697fb620af44b61a6e4ade3eb
linuxlite <eze.rey92@gmail.com>
2022-12-12 18:24:14 -0300

    datos

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,10 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
   username: String;
+  @IsNotEmpty()
   password: String;
+  @IsEmail()
   email: String;
   createdAt: Date;
 }
