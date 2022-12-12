bf50b59faf6b67048807dc81e5efa5e8a705bb77
linuxlite <eze.rey92@gmail.com>
2022-12-12 19:30:24 -0300

    generando el auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, MongooseModule.forRoot('mongodb://localhost/api')],
+  imports: [UsersModule, MongooseModule.forRoot('mongodb://localhost/api'), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
