d02cba1e5f42067b4a09352162ed2ea354c96d81
linuxlite <eze.rey92@gmail.com>
2022-12-16 13:24:02 -0300

    interface

--- src/profiles/dto/create-profile.dto.ts
+++ src/profiles/dto/create-profile.dto.ts
@@ -1,1 +1,30 @@
-export class CreateProfileDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateProfileDto {
+  @ApiProperty()
+  birthday: String;
+  @ApiProperty()
+  gender: String;
+  @ApiProperty()
+  image: String;
+  @ApiProperty()
+  imageHeader: String;
+  @ApiProperty()
+  title: String;
+  @ApiProperty()
+  bio: String;
+  @ApiProperty()
+  likes: String;
+  @ApiProperty()
+  dislikes: String;
+  @ApiProperty()
+  publicEmail: String;
+  @ApiProperty()
+  userId: String;
+  @ApiProperty()
+  sentimentalId: String;
+  @ApiProperty()
+  countryId: String;
+  @ApiProperty()
+  distributionId: String;
+}
---
+++ src/profiles/interface.crear.profile.ts
@@ -0,0 +1,15 @@
+export interface ProfileInterFace {
+  birthday: String;
+  gender: String;
+  image: String;
+  imageHeader: String;
+  title: String;
+  bio: String;
+  likes: String;
+  dislikes: String;
+  publicEmail: String;
+  userId: String;
+  sentimentalId: String;
+  countryId: String;
+  distributionId: String;
+}
