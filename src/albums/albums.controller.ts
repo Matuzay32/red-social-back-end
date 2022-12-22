f101b70666ef2c30c7da8563e2b3e3cf8e3c6bf1
linuxlite <eze.rey92@gmail.com>
2022-12-22 15:41:26 -0300

    multer por comenzar

--- src/albums/albums.controller.ts
+++ src/albums/albums.controller.ts
@@ -6,8 +6,16 @@ import {
   Patch,
   Param,
   Delete,
+  UseInterceptors,
+  HttpVersionNotSupportedException,
+  HttpStatus,
+  UploadedFiles,
+  Res,
 } from '@nestjs/common';
+import { AnyFilesInterceptor } from '@nestjs/platform-express';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { join } from 'path';
+import { of } from 'rxjs';
 import { AlbumsService } from './albums.service';
 import { CreateAlbumDto } from './dto/create-album.dto';
 import { UpdateAlbumDto } from './dto/update-album.dto';
@@ -41,4 +49,41 @@ export class AlbumsController {
   remove(@Param('id') id: string) {
     return this.albumsService.remove(+id);
   }
+
+  // @Post('files')
+  // @UseInterceptors(
+  //   AnyFilesInterceptor({
+  //     storage: diskStorage({
+  //       destination: './uploads',
+  //       filename: (req, file, callback) => {
+  //         callback(null, `${uuidv4()}.jpg`);
+  //       },
+  //     }),
+  //     fileFilter: (req, file, callback) => {
+  //       const nameOriginal = file.originalname.toLocaleLowerCase();
+  //       if (!nameOriginal.match(/(.gif|.png|.jpg|.jpeg)$/)) {
+  //         return callback(
+  //           new HttpVersionNotSupportedException({
+  //             status: HttpStatus.NOT_FOUND,
+  //             error: `Uno de los archivos tiene una extension no valida. Validas: gif png jpg jpeg`,
+  //           }),
+  //           false,
+  //         );
+  //       }
+  //       callback(null, true);
+  //     },
+  //   }),
+  // )
+  // uploadFiles(@UploadedFiles() files: Array<Express.Multer.File>) {
+  //   return files.map((item) => {
+  //     return { nombre: item.filename };
+  //   });
+  // }
+
+  // @Get('uploads/:imagename')
+  // findProfileImage(@Param() params, @Res() res) {
+  //   const { imagename } = params;
+
+  //   return of(res.sendFile(join(process.cwd(), `uploads/${imagename}`)));
+  // }
 }
