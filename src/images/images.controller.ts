b1eebddf41fac4d092fa81dadb3486543b3a8ce5
linuxlite <eze.rey92@gmail.com>
2023-01-02 14:05:23 -0300

    pase la carga de imagenes al controlador de imagenes

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -6,11 +6,16 @@ import {
   Patch,
   Param,
   Delete,
+  UseInterceptors,
 } from '@nestjs/common';
 import { ImagesService } from './images.service';
 import { CreateImageDto } from './dto/create-image.dto';
 import { UpdateImageDto } from './dto/update-image.dto';
-import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
+import { ApiTags, ApiBearerAuth, ApiConsumes } from '@nestjs/swagger';
+import { AnyFilesInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+
+import { v4 as uuidv4 } from 'uuid';
 
 @ApiTags('images')
 @ApiBearerAuth()
@@ -46,5 +51,62 @@ export class ImagesController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.imagesService.remove(id);
+  }
+
+  @ApiBody({
+    required: true,
+    type: 'multipart/form-data',
+    schema: {
+      type: 'object',
+      properties: {
+        files: {
+          type: 'array',
+          items: {
+            type: 'string',
+            format: 'binary',
+          },
+        },
+      },
+    },
+  })
+  @ApiConsumes('multipart/form-data')
+  @Post('files')
+  @UseInterceptors(
+    AnyFilesInterceptor({
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, callback) => {
+          const extension = file.originalname
+            .toLowerCase()
+            .match(/(.gif|.png|.jpg|.jpeg|.mp4)$/);
+
+          callback(null, `${uuidv4()}${extension[0]}`);
+        },
+      }),
+      fileFilter: (req, file, callback) => {
+        const nameOriginal = file.originalname.toLocaleLowerCase();
+        if (!nameOriginal.match(/(.gif|.png|.jpg|.jpeg|.mp4)$/)) {
+          return callback(
+            new HttpVersionNotSupportedException({
+              status: HttpStatus.NOT_FOUND,
+              error: `Uno de los archivos tiene una extension no valida. Validas: gif png jpg jpeg`,
+            }),
+            false,
+          );
+        }
+        callback(null, true);
+      },
+    }),
+  )
+  uploadFiles(@UploadedFiles() files: Array<any>) {
+    return files.map((item) => {
+      return item.filename;
+    });
+  }
+
+  @Get('uploads/:id')
+  @ApiResponse({ type: Buffer })
+  findProfileImage(@Param('id') id: string, @Res() res) {
+    return of(res.sendFile(join(process.cwd(), `uploads/${id}`)));
   }
 }
